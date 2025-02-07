1886fdd8de503534203a74dbd0615f26bf148060
IcaroCos19 <88943570+IcaroCos19@users.noreply.github.com>
2025-02-07 16:08:18 -0300

    Adicionando Arquivos

---
+++ src/App.java
@@ -0,0 +1,59 @@
+public class App {
+    public static void main(String[] args)  {
+        int [][] ilha = {
+            {0, 1, 1, 1, 0},
+            {0, 0, 1, 1, 0},
+            {0, 1, 1, 1, 0},
+            {0, 1, 1, 1, 0},
+            {0, 0, 1, 1, 0},
+            {0, 0, 0, 1, 0},
+            
+        };
+        
+        int perimetro = calculaPerimetro(ilha);
+        int area = calcularArea(ilha);
+        System.out.println("Perimetro da Ilha: "+ perimetro);
+        System.out.println("Area da Ilha: "+ area);
+    }
+
+    public static int calculaPerimetro(int[][] ilha){
+        int perimetro = 0;
+        int linhas = ilha.length;
+        int colunas = ilha[0].length;
+
+        for(int i = 0; i < linhas; i++){
+            for(int j = 0; j< colunas; j++){
+                if(ilha[i][j] == 1){
+                    if(i == 0 || ilha[i-1][j] == 0){
+                        perimetro++;
+                    }
+                    if(i == linhas - 1|| ilha[i + 1][j] == 0){
+                        perimetro++;
+                    }
+                    if(j == 0 || ilha[i][j - 1] == 0){
+                        perimetro++;
+                    }
+                    if(j == colunas || ilha[i][j + 1] == 0){
+                        perimetro++;
+                    }
+                }
+            }
+        }
+        return perimetro;
+    }
+
+    public static int calcularArea(int[][] ilha){
+        int area = 0;
+        int linhas = ilha.length;
+        int colunas = ilha[0].length;
+
+        for(int i = 0; i < linhas; i++){
+            for(int j = 0; j < colunas; j ++){
+                if(ilha[i][j] == 1){
+                    area ++;
+                }
+            }
+        }
+        return area;
+    }
+}
